75e50e6a46f1374ee8306e220276e1bd642a20b2
vitsto <stolyarov.vitalij@list.ru>
2023-01-20 09:57:00 +1000

    fixed

--- src/Main.java
+++ src/Main.java
@@ -2,30 +2,36 @@ import java.util.Comparator;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = new LinkedList<>(List.of(1, 2, 3, 4, 5, 6, 7, 8, 9));
+
 //        List<Integer> list = new LinkedList<>(); // выдает null,null
-        findMinMax(list.stream(), Integer::compareTo, (integer, integer2) -> {
-            System.out.println("Min: " + integer);
-            System.out.println("Max: " + integer2);
+        findMinMax(list.stream(), Integer::compareTo, (min, max) -> {
+            System.out.println("Min: " + min);
+            System.out.println("Max: " + max);
+        });
+
+        List<String> list2 = new LinkedList<>(List.of("a", "ab", "abc", "abcd"));
+        findMinMax(list2.stream(), String::compareTo, (min, max) -> {
+            System.out.println("Min: " + min);
+            System.out.println("Max: " + max);
         });
         System.out.println(countEven(list));
     }
 
-    public static void findMinMax(Stream<? extends Integer> stream,
-                                  Comparator<? super Integer> comparator,
-                                  BiConsumer<? super Integer, ? super Integer> minMaxConsumer) {
-        List<Integer> integers = stream.map(Integer::valueOf).toList();
-        Integer min = integers.stream().min(comparator).orElse(null);
-        Integer max = integers.stream().max(comparator).orElse(null);
-        if (min == null) {
+    public static <T> void findMinMax(Stream<? extends T> stream,
+                                      Comparator<? super T> comparator,
+                                      BiConsumer<? super T, T> minMaxConsumer) {
+        List<? extends T> list = stream.sorted(comparator).toList();
+        if (list.isEmpty()) {
             minMaxConsumer.accept(null, null);
-        } else {
-            minMaxConsumer.accept(min, max);
         }
+
+        minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
     }
 
     public static long countEven(List<Integer> list) {
